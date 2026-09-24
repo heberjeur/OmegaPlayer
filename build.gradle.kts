7d1ddde9f645edc059ae1ce7ab5e7142e13f4a21
heberjeur <11852634+heberjeur@users.noreply.github.com>
2026-09-24 15:59:19 +0100

    Security: Force transitive dependency versions to resolve remaining Dependabot alerts

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,3 +11,29 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt) apply false
 }
+
+buildscript {
+    configurations.all {
+        resolutionStrategy {
+            force("org.bouncycastle:bcprov-jdk18on:1.85")
+            force("org.bouncycastle:bcpkix-jdk18on:1.85")
+            force("org.bitbucket.b_c:jose4j:0.9.6")
+            force("org.jdom:jdom2:2.0.6.1")
+            force("org.apache.commons:commons-lang3:3.18.0")
+            force("org.apache.httpcomponents:httpclient:4.5.13")
+        }
+    }
+}
+
+allprojects {
+    configurations.all {
+        resolutionStrategy {
+            force("org.bouncycastle:bcprov-jdk18on:1.85")
+            force("org.bouncycastle:bcpkix-jdk18on:1.85")
+            force("org.bitbucket.b_c:jose4j:0.9.6")
+            force("org.jdom:jdom2:2.0.6.1")
+            force("org.apache.commons:commons-lang3:3.18.0")
+            force("org.apache.httpcomponents:httpclient:4.5.13")
+        }
+    }
+}
